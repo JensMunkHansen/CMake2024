e32fdb4824ad666c31807027fb72adf487834365
Jens Munk Hansen <jens.hansen@shape.com>
2024-02-09 12:44:28 +0100

    Everything upper-case

---
+++ DeepLibraryLarge/Deep/include/Deep/Deep.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Deep/Deep_export.h"
+#include <cstddef>
+
+namespace Deep
+{
+  TS_DEEP_EXPORT const char* Version();
+}
---
+++ DeepLibraryLarge/Deep/src/Deep.cxx
@@ -0,0 +1,11 @@
+#include <Deep/Deep.h>
+#include <Deep/Config.h>
+
+namespace deep {
+
+const char* Version()
+{
+  return DEEP_VERSION_STRING;
+}
+  
+}
