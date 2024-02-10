87f7faa9dfd0d9b035b5f64fc618e3a2aa9be876
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-02-10 18:31:47 +0100

    clean script

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -Rf ./install
-rm -Rf ./Library/release
-rm -Rf ./Library/debug
-rm -Rf ./LibraryUsage/build
-rm -Rf ./Library/build
+find . -name _CPack_Packages -type d | xargs -i -t rm -Rf {}
+find . -name build -type d | xargs -i -t rm -Rf {}
+find . -name release -type d | xargs -i -t rm -Rf {}
+find . -name debug -type d | xargs -i -t rm -Rf {}
+find . -name install -type d | xargs -i -t rm -Rf {}
