e1d2f7cce4e3c8ed9768ac0b549089e661067f93
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-26 20:47:07 +0200

    super cool interface parameters

--- LibraryUsage/configure.sh
+++ LibraryUsage/configure.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cmake -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install
-cmake -B build/release -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install
+#cmake -B build/debug -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install
+#cmake -B build/release -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install
+
+#cmake -B build/debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install 
+cmake -B build/release -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_PREFIX_PATH=$(pwd)/../Library/install
