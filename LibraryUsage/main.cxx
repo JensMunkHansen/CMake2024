0a32bad5394b5f4d3c0f51e49e968b6fcd5ffe45
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-26 20:16:31 +0200

    ..

--- LibraryUsage/main.cxx
+++ LibraryUsage/main.cxx
@@ -1,9 +1,26 @@
 // TODO: When installed, it is Foo/Foo.h, else Foo.h
 #include <Foo/Foo.h>
+
+// Header-only
+#include <Bar/Function.hpp>
+
 #include <iostream>
 
 int main(int, char*[])
 {
   std::cout << Library::Version() << std::endl;
+
+  // Stupid copy-paste code
+  volatile size_t state = 0;
+  struct State
+  {
+    size_t a, b, c;
+  };
+  State state2 = { 0, 0, 0 };
+
+  size_t i = 2;
+  myLib::Function<void()> fun = [&state, state2, i]() { state = i; };
+  fun();
+  
   return 0;
 }
