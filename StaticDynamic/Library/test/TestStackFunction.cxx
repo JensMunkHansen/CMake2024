6175e2271cc84e3272bae22202235d1ce252ffe3
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-28 21:16:07 +0200

    CMakePresets.json for WIN32

--- StaticDynamic/Library/test/TestStackFunction.cxx
+++ StaticDynamic/Library/test/TestStackFunction.cxx
@@ -1,9 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
 
-//#define CATCH_CONFIG_MAIN
-//#include <catch2/catch.hpp>
-
 #include <sps/functional>
 
 TEST_CASE("sample test", "[ctor]")
