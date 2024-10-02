374f58744a2f1cb63dcec4c50a9a8c1df2095749
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-02 16:18:21 +0200

    Initial version

---
+++ vtkSPS/Common/Core/spsVersion.h
@@ -0,0 +1,49 @@
+#ifndef spsVersion_h
+#define spsVersion_h
+
+#include "spsCommonCoreModule.h" // For export macro
+#include "spsVersionMacros.h"
+#include "vtkObject.h"
+
+#include "vtkVersionFull.h"
+#include "vtkVersionMacros.h" // For VTK version
+
+class SPSCOMMONCORE_EXPORT spsVersion : public vtkObject
+{
+public:
+  static spsVersion* New();
+  vtkTypeMacro(spsVersion, vtkObject)
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  /**
+   * Return the version of vtk this object is a part of.
+   * A variety of methods are included. GetVTKSourceTsVersion returns a string
+   * with an identifier which timestamps a particular source tree.
+   */
+  static const char* GetSpsVersionFull();
+  static int GetSpsMajorVersion() { return SPS_MAJOR_VERSION; }
+  static int GetSpsMinorVersion() { return SPS_MINOR_VERSION; }
+  static int GetSpsBuildVersion() { return SPS_BUILD_VERSION; }
+  static const char* GetSpsSourceVersion() { return SPS_SOURCE_VERSION; }
+
+  static const char* GetVTKVersionFull();
+  static int GetVTKMajorVersion() { return VTK_MAJOR_VERSION; }
+  static int GetVTKMinorVersion() { return VTK_MINOR_VERSION; }
+  static int GetVTKBuildVersion() { return VTK_BUILD_VERSION; }
+  static const char* GetVTKSourceVersion() { return VTK_SOURCE_VERSION; }
+
+protected:
+  spsVersion() = default; // ensure constructor/destructor protected
+  ~spsVersion() override = default;
+
+private:
+  spsVersion(const spsVersion&) = delete;
+  void operator=(const spsVersion&) = delete;
+};
+
+extern "C"
+{
+  SPSCOMMONCORE_EXPORT const char* GetSpsVersion();
+}
+
+#endif
