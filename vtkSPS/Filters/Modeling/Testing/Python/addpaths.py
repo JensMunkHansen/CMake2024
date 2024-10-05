b205ad90d2e429890123de99760735b92d0f5460
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-05 16:25:10 +0200

    Common include for python

--- vtkSPS/Filters/Modeling/Testing/Python/addpaths.py
+++ vtkSPS/Filters/Modeling/Testing/Python/addpaths.py
@@ -5,9 +5,7 @@ import os
 
 # Home-made additions to VTK
 filedir = os.path.dirname(os.path.realpath(__file__))
-if os.name == "posix":
-  filepath = os.path.join(filedir, 4*"../" + "build/lib/python%d.%d/site-packages" % (sys.version_info.major, sys.version_info.minor))
-  sys.path.insert(0, filepath)
-else:
-  filepath = os.path.join(filedir, "../../../../build/bin/Lib/site-packages")
-  sys.path.insert(0, filepath)
+filepath = os.path.join(filedir, 4*"../")
+sys.path.insert(0, filepath)
+
+import spspython
