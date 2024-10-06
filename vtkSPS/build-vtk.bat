31b7ba9baff66efbcf3b3ca74dd4729db2cc1136
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-06 18:43:52 +0200

    ..

--- vtkSPS/build-vtk.bat
+++ vtkSPS/build-vtk.bat
@@ -26,6 +26,8 @@ if %myArg% == DEBUG (
 mkdir build_Release
 cmake -HC:/VTK/VTK -BC:/VTK/build_Release -G "Visual Studio 17 2022" -A "x64" -DCMAKE_CXX_MP_FLAG=ON -DVTK_WRAP_PYTHON=ON -DVTK_WINDOWS_PYTHON_DEBUGGABLE=ON
 
+set PATH="C:/Program Files/Python312";%PATH%
+
 call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 cd build_Release
 
