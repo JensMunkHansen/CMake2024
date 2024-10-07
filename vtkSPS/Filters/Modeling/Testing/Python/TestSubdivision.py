adf31ba1493fd2c7b0e1896e0c1d35206d6fdb04
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-07 22:11:05 +0200

    Regression test

--- vtkSPS/Filters/Modeling/Testing/Python/TestSubdivision.py
+++ vtkSPS/Filters/Modeling/Testing/Python/TestSubdivision.py
@@ -1,64 +1,59 @@
 import addpaths
 
 from spsmodules.spsFiltersModeling import spsSimpleTriangleSubdivisionFilter
+from spsmodules.util.scene_utils import (
+    vtk_color_cells_randomly,
+    vtk_color_points_randomly)
+
+# noinspection PyUnresolvedReferences
+import vtkmodules.vtkInteractionStyle
+# noinspection PyUnresolvedReferences
+import vtkmodules.vtkRenderingOpenGL2
+
 from vtkmodules.vtkFiltersSources import vtkSphereSource
-from vtkmodules.vtkCommonCore import (
-  vtkUnsignedCharArray,
-  vtkRandomPool)
+from vtkmodules.vtkRenderingCore import (
+    vtkRenderWindow,
+    vtkActor,
+    vtkRenderWindowInteractor,
+    vtkRenderer,
+    vtkPolyDataMapper)
 
 sphere = vtkSphereSource()
 sphere.Update()
 inPolyData = sphere.GetOutput()
 
-# TODO: Handle cell data interpolation
-colors = vtkUnsignedCharArray()
-colors.SetNumberOfComponents(3)
-colors.SetNumberOfTuples(inPolyData.GetNumberOfPoints())
-colors.SetName("Colors")
-
-rnd = vtkRandomPool()
-rnd.PopulateDataArray(colors, 0, 255)
-
-inPolyData.GetPointData().SetScalars(colors)
-inPolyData.GetPointData().SetActiveScalars("Colors")
+vtk_color_points_randomly(inPolyData)
 
 nInputPoints = inPolyData.GetNumberOfPoints()
 nInputCells = inPolyData.GetNumberOfCells()
 
 subdivision = spsSimpleTriangleSubdivisionFilter()
-#subdivision.DebugOn()
 nSubDivisions = 1
 subdivision.SetNumberOfSubdivisions(nSubDivisions)
 subdivision.SetInputData(inPolyData)
 subdivision.Update()
 
 outPolyData = subdivision.GetOutput()
-#outPolyData.GetPointData().SetActiveScalars("Colors")
 
-cellColors = vtkUnsignedCharArray()
-cellColors.SetNumberOfComponents(3)
-cellColors.SetNumberOfTuples(outPolyData.GetNumberOfCells())
-rnd.PopulateDataArray(cellColors, 0, 255)
-outPolyData.GetCellData().SetScalars(cellColors)
+vtk_color_cells_randomly(outPolyData)
 
 nOutputPoints = outPolyData.GetNumberOfPoints()
 nOutputCells = outPolyData.GetNumberOfCells()
 assert(nOutputPoints == nInputPoints + 0.5*(3**(nSubDivisions) - 1) * nInputCells)
 assert(nOutputCells == nInputCells * 3**(nSubDivisions))
 
-import vtk
-renderer = vtk.vtkRenderer()
-mapper = vtk.vtkPolyDataMapper()
+renderer = vtkRenderer()
+mapper = vtkPolyDataMapper()
 mapper.SetInputData(outPolyData)
 mapper.SetScalarModeToUseCellData()
 
-actor = vtk.vtkActor()
+actor = vtkActor()
 actor.SetMapper(mapper)
 
 renderer.AddActor(actor)
-rwin = vtk.vtkRenderWindow()
+rwin = vtkRenderWindow()
 rwin.AddRenderer(renderer)
 rwin.Render()
-iren = vtk.vtkRenderWindowInteractor()
+iren = vtkRenderWindowInteractor()
 iren.SetRenderWindow(rwin)
 iren.Start()
--- vtkSPS/Wrapping/Python/spsmodules/util/scene_utils.py
+++ vtkSPS/Wrapping/Python/spsmodules/util/scene_utils.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
-from vtkmodules.vtkCommonCore import vtkUnsignedCharArray
+from vtkmodules.vtkCommonColor import vtkNamedColors
+from vtkmodules.vtkCommonCore import (
+  vtkUnsignedCharArray,
+  vtkRandomPool)
 from vtkmodules.vtkCommonDataModel import vtkPolyData
+
+def vtk_next_named_color(step=2):
+  colors = vtkNamedColors()
+  colorNames = colors.GetColorNames().split("\n")
+  if not (vtk_next_named_color.iColor < len(colorNames)):
+    vtk_next_named_color.iColor = 0
+  colorName = colorNames[vtk_next_named_color.iColor]
+  vtk_next_named_color.iColor = vtk_next_named_color.iColor + step
+  return colors.GetColor3d(colorName)
+vtk_next_named_color.iColor = 0
 
 def vtk_color_all_cells(polyData:vtkPolyData, color = None):
     colors = vtkUnsignedCharArray()
@@ -14,6 +27,27 @@ def vtk_color_all_cells(polyData:vtkPolyData, color = None):
     colors.FillComponent(2, 255*color[2])
     polyData.GetCellData().SetScalars(colors)
 
+def vtk_color_points_randomly(polyData:vtkPolyData):
+  colors = vtkUnsignedCharArray()
+  colors.SetNumberOfComponents(3)
+  colors.SetNumberOfTuples(polyData.GetNumberOfPoints())
+  colors.SetName("Colors")
+  rnd = vtkRandomPool()
+  rnd.PopulateDataArray(colors, 0, 255)
+  polyData.GetPointData().SetScalars(colors)
+  polyData.GetPointData().SetActiveScalars("Colors")
+
+def vtk_color_cells_randomly(polyData:vtkPolyData):
+  cellColors = vtkUnsignedCharArray()
+  cellColors.SetNumberOfComponents(3)
+  cellColors.SetNumberOfTuples(polyData.GetNumberOfCells())
+  cellColors.SetName("Colors")
+
+  rnd = vtkRandomPool()
+  rnd.PopulateDataArray(cellColors, 0, 255)
+
+  polyData.GetCellData().SetScalars(cellColors)
+    
 
 # Local variables: #
 # tab-width: 2 #
