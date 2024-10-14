9124b2b175fd9c87d0ed37e438f7deb6de765afb
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-14 09:13:57 +0200

    .

--- vtkSPS/sitecustomize.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-PKG_ROOT = os.environ.get("PKG_ROOT")
-os.add_dll_directory(f"{PKG_ROOT}/ArtifactoryInstall/WindowsShared/VTK/bin")
-sys.path.insert(0, f"{PKG_ROOT}/ArtifactoryInstall/WindowsShared/VTK/lib/site-packages")
-
-
