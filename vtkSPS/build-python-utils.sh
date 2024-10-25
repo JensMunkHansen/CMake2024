1a0ced09a997f8b3d40d71ea10687c563a05f19f
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-10-25 09:41:39 +0200

    box polydata

---
+++ vtkSPS/build-python-utils.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -r ./Wrapping/Python/spsmodules/util/*.py ./build/bin/Lib/site-packages/spsmodules/util/
+cp -r ./Wrapping/Python/spsmodules/util/*.py ./build/lib/python3.11/site-packages/spsmodules/util/
