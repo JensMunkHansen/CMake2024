55b3bee5fb105dd0d04f2ee346fdcf6f1625bada
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-16 05:11:12 +0100

    ..

--- vtkSPS/wasm.sh
+++ vtkSPS/wasm.sh
@@ -23,7 +23,7 @@ echo "Detected OS: $OS_TYPE"
 (
     source "$HOME/github/emsdk/emsdk_env.sh"
     if [ "$OS_TYPE" == "Debian" ]; then
-	emcmake cmake -S. -Bbuild -DVTK_DIR=/home/jmh/github/VTKCMake/install/lib/cmake/vtk  -DCMAKE_BUILD_TYPE=Release -DSPS_BUILD_TESTING=ON
+	emcmake cmake -S. -Bbuild -DVTK_DIR=/home/jmh/github/VTKCMake/install/lib/cmake/vtk  -DCMAKE_BUILD_TYPE=Release -DSPS_BUILD_TESTING=ON -DBUILD_TESTING=ON
     else
 	emcmake cmake -S. -Bbuild -DVTK_DIR=$TSPKG_ROOT/ArtifactoryInstall/Wasm32StaticMT/Release/lib/cmake/vtk -DVTKEmbinding_BUILD_EXAMPLES=ON -DVTKEmbinding_ENABLE_WEBGPU=OFF -DVTKEmbinding_BUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DVTKEmbinding_ENABLE_THREADING=ON
     fi	
